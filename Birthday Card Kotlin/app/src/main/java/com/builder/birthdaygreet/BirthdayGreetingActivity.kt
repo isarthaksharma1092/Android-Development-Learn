161bf7bf34bb22c28fce48d5aa5e1d42d30e4d1f
Sarthak Sharma <isarthaksharma1092@gmail.com>
2023-02-18 19:01:39 +0530

    Birthday Card 1st

---
+++ Birthday Card Kotlin/app/src/main/java/com/builder/birthdaygreet/BirthdayGreetingActivity.kt
@@ -0,0 +1,13 @@
+package com.builder.birthdaygreet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+
+    }
+}
---
+++ Birthday Card Kotlin/app/src/main/java/com/builder/birthdaygreet/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.builder.birthdaygreet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.reflect.Method
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        method 1:
+//        createBirthdayCard.setOnClickListener{
+//
+//        }
+    }
+
+
+//        Method 2: Android:onClock="method_name"
+    fun createBirthdayCard(view: View)
+    {
+//        Toast message is displayed on app
+//        example 1
+//        Toast.makeText(this,"Button was Clicked",Toast.LENGTH_LONG).show()
+
+//        example 2
+        val name = nameInput.editableText.toString()
+        Toast.makeText(this,"Greeting for $name is being generated. ",Toast.LENGTH_LONG).show()
+
+    }
+}
